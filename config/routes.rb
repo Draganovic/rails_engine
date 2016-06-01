7bd8ba5d7f2c87fa1c8e1f60ca564e735a8ff64f
Admir Draganovic <admir.draganovic@gmail.com>
2016-06-01 15:38:47 -0600

    Add show for customers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json} do
       resources :merchants, only: [:index]
-      resources :customers, only: [:index]
+      resources :customers, only: [:index, :show]
       resources :items, only: [:index, :show, :create, :update, :destroy]
       resources :invoices, only: [:index]
       resources :invoice_items, only: [:index]
--- spec/controllers/api/v1/customers_controller_spec.rb
+++ spec/controllers/api/v1/customers_controller_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe Api::V1::CustomersController, type: :controller do
 
     expect(customers.first["first_name"]).to eq "Bob"
   end
+
+  it "can view one customer" do
+    customer1 = Customer.create(first_name: "Bob", last_name: "Dillan")
+    customer2= Customer.create(first_name: "Tom", last_name: "Tompson")
+
+    id = Customer.last.id
+
+    get "show", id: id, format: :json
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(customer["id"]).to eq id
+  end
 end
