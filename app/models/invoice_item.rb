e3ad063395042688234a0a7b2e526c963dae0f6b
Admir Draganovic <admir.draganovic@gmail.com>
2016-06-02 00:06:00 -0600

    Add merchant relationship end points
    *Added merchant invoices
    *Added merchant items
    *Added model relationships

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,6 @@
 class InvoiceItem < ActiveRecord::Base
+  belongs_to :item
+  belongs_to :invoice
 
   def random
     num = rand(InvoiceItem.count)
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,4 @@
 class Merchant < ActiveRecord::Base
-has_many :items
-
-
+  has_many :items
+  has_many :invoices
 end
