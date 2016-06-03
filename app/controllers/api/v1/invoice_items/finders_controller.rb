faa43a682de255cd8d9f7f9d5d601bb617e11408
Admir Draganovic <admir.draganovic@gmail.com>
2016-06-02 19:16:33 -0600

    Add before action to application controller

--- app/controllers/api/v1/invoice_items/finders_controller.rb
+++ app/controllers/api/v1/invoice_items/finders_controller.rb
@@ -2,24 +2,13 @@ class Api::V1::InvoiceItems::FindersController < ApplicationController
   respond_to :json
 
   def index
-    if invoice_item_params[:unit_price]
-      unit_price = (invoice_item_params[:unit_price].to_f * 100).to_i
-      respond_with InvoiceItem.where(unit_price: unit_price)
-    else
-      respond_with InvoiceItem.where(invoice_item_params)
-    end
+    respond_with InvoiceItem.where(invoice_item_params)
   end
 
   def show
-    if invoice_item_params[:unit_price]
-      unit_price = (invoice_item_params[:unit_price].to_f * 100).to_i
-      invoice_items = InvoiceItem.where(unit_price: unit_price)
-      respond_with invoice_items.first
-    else
     invoice_items = InvoiceItem.where(invoice_item_params)
     respond_with invoice_items.first
   end
-end
 
   private
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :price_to_dollars
+
+  def price_to_dollars
+    if params[:unit_price]
+      params[:unit_price] = (params[:unit_price].to_f * 100).round
+    end
+  end
 end
